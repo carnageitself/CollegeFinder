f6a0171eb1cc7e48f0a3195896e81fbd88224c94
Akash Kumar <Aakashdev24@gmail.com>
2023-07-30 22:32:45 +0530

    Navbar corrected

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,12 +1,15 @@
+
 import Link from "next/link";
 import React from "react";
 import logo from "../../../public/next.svg"
+import Image from "next/image";
 
 interface navItem {
   id: number;
   title: string;
   url: string;
 }
+
 const Navbar: React.FC = () => {
 
 
@@ -38,13 +41,19 @@ const Navbar: React.FC = () => {
     <div className="navbar w-full h-20 bg-[#121212] text-white flex justify-between items-center">
         <div className="left ml-10">
           <Link href="/">
-          <img src={logo} alt="" className="cursor-pointer h-12 w-12"/>
+          <Image src={logo} alt="" className="cursor-pointer h-12 w-12"/>
           </Link>
         </div>
         <div className="center">
-          {nav.map((item) => (
-            <Link key={item.id} href={item.url} className="p-10 text-lg uppercase">{item.title}</Link>
-          ))}
+          {/* {nav.map((item) => ( */}
+            <Link  href={'/'} className="p-10 text-lg uppercase">Home</Link>
+            <Link  href={'/topcolleges'} className="p-10 text-lg uppercase">TopColleges</Link>
+
+            <Link  href={'/admission'} className="p-10 text-lg uppercase">Admission</Link>
+            <Link  href={'/about'} className="p-10 text-lg uppercase">About</Link>
+            <Link  href={'/contact'} className="p-10 text-lg uppercase">Contact</Link>
+
+          {/* ))} */}
         </div>
         <div className="right mr-10">
           <button>Log out</button>

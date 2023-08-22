c1a7f02efe12fecdafa9574136017b9d5c0b4690
Namansingh98 <naman.rathore98@outlook.com>
2023-08-22 20:57:28 +0530

    responsive done bug still exist

--- app/components/hero/Hero.tsx
+++ app/components/hero/Hero.tsx
@@ -13,51 +13,49 @@ const Hero: React.FC = () => {
     id: 1,
     name: "Yash Harale",
   };
-const animatedWord="Unlock Your Potential, Embrace Your Future: Your Journey Starts Here " 
+  const animatedWord =
+    "Unlock Your Potential, Embrace Your Future: Your Journey Starts Here ";
 
   return (
-    <div className="h-{100} w-full">
-      <div className="grid grid-cols-2 ">
-        <motion.div className="m-5 relative main__center"
-
-          initial={{ opacity: 0, x: -10}}
-          animate={{ x: -10, y: -10 ,opacity:1}}
+    <div className="h-{100} w-full ">
+      <div className="flex items-center justify-center  md:flex-row sm:flex-col res">
+        <motion.div
+          className="m-5 "
+          initial={{ opacity: 0, x: -10 }}
+          animate={{ x: -10, y: -10, opacity: 1 }}
           transition={{ delay: 0.9 }}
-          exit={{ x: 0, y: 0 ,opacity:0}}
+          exit={{ x: 0, y: 0, opacity: 0 }}
         >
-          <h1 className="text-3xl text-center">
+          <h1 className="text-3xl text-center top-[40%] relative">
             {" "}
             "Unlock Your Potential, Embrace Your Future: Your Journey Starts
             Here !"
           </h1>
-          <div className="custom__btn mt-5">
-            <a className="anchor" href="#">
+          <div className="top-[40%] relative pt-5 flex justify-center">
+            <button className="bg-gray-500 rounded-md px-5 py-2 text-white   items-center ">
               {" "}
               Register
-            </a>
+            </button>
           </div>
         </motion.div>
         <motion.div
-          className="img m-6 "
-          initial={{ opacity: 0, x: 10,y:0}}
-          animate={{ x: 10, y: 15 ,opacity:1}}
-          transition={{ delay: .4 }}
-          exit={{ x: 0, y: 0 ,opacity:0}}
+          className="m-6 flex justify-center items-center sm:columns-1 "
+          initial={{ opacity: 0, x: 10, y: 0 }}
+          animate={{ x: 10, y: 15, opacity: 1 }}
+          transition={{ delay: 0.4 }}
+          exit={{ x: 0, y: 0, opacity: 0 }}
         >
           <Image
             src="./leftmain.svg"
             width={600}
             height={600}
             alt="img"
-            className="transform -scale-x-100"
+            className="transform -scale-x-100 "
           />
         </motion.div>
-        
       </div>
     </div>
   );
 };
 
 export default Hero;
-
-
